f8d34dd7bd3be807930877fff73a82b2535b5f35
mozin <mohsinvindhani@gmail.com>
2023-11-03 23:04:26 -0400

    [TEST] project delete endpoint

--- src/models/projects/projects_controller.py
+++ src/models/projects/projects_controller.py
@@ -1,6 +1,7 @@
 from flasgger import swag_from
 from flask import Blueprint, request, jsonify
 from flask_expects_json import expects_json
+from flask_jwt_extended import jwt_required
 
 from src.common.jwt_helper import create_token_obj_bundle, get_user_identity
 from src.common.utils import Utils
@@ -11,6 +12,7 @@ projects_blueprint = Blueprint("projects", __name__)
 
 
 @projects_blueprint.route("/create", methods=["POST"])
+@jwt_required()
 @expects_json(
     {
     'type': 'object',
@@ -45,9 +47,9 @@ def create_project():
            type: "boolean"
    responses:
     200:
-      description: User object with token
+      description: Success message
     400:
-      description: Invalid input | MISSING_INPUT_KEYS | USER_NOT_FOUND | INCORRECT_PASSWORD
+      description: Invalid input | MISSING_INPUT_KEYS
    '''
     user_id = get_user_identity()
     user = User.find_user_by_id(user_id)
@@ -70,3 +72,33 @@ def create_project():
 
     return jsonify({"response" : "Success"}), 200
 
+
+
+@projects_blueprint.route("/delete/<project_id>", methods=["GET"])
+@jwt_required()
+def delete_project(project_id):
+    '''Get quiz attempt details
+    ---
+   tags:
+   - "Quiz Attempt"
+   consumes:
+   - "application/json"
+   security: [ { 'bearerAuth': [] } ]
+   parameters:
+   - name: quiz_id
+     in: path
+     type: string
+     required: true
+
+   responses:
+    200:
+      description: Delete Project
+    400:
+      description: PROJECT_NOT_FOUND
+    '''
+
+    if Project.find_project_by_id(project_id) is None:
+        return jsonify({"response": "PROJECT_NOT_FOUND"}), 400
+
+    Project.delete_project_by_id(project_id)
+    return jsonify({"response" : "Success"}), 200
